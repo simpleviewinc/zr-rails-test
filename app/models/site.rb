cdddb1b203613664606087e6f18c01ba01c3292c
Duggan Roberts <dugganroberts@gmail.com>
2019-08-11 22:43:15 -0500

    fixed bug when adding first page to sites, added dependent destroy on Site model

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,9 +1,18 @@
 class Site < ApplicationRecord
   belongs_to :user
 
-  has_many :pages
+  has_many :pages, dependent: :destroy
 
   accepts_nested_attributes_for :pages, allow_destroy: true
 
   validates_presence_of :name, :description, :domain, :user
+
+  # before_destroy :destroy_pages
+
+ private
+
+ # def destroy_pages
+ #   self.pages.destroy_all
+ # end
+
 end
